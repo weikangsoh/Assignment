08a3486988e9c53d67d2150d27beeacf21b472df
weikangsoh <103031263+weikangsoh@users.noreply.github.com>
2022-04-07 17:06:40 +0800

    Update IndexController.java

--- workshop01/src/main/java/com/example/workshop01/controller/IndexController.java
+++ workshop01/src/main/java/com/example/workshop01/controller/IndexController.java
@@ -31,12 +31,22 @@ public class IndexController {
     public String index2(@RequestParam String name, @RequestParam String mail) {
         IndexModel newData = new IndexModel();
         newData.setData(name, mail);
-        return newData.getName() + "\n" + newData.getMail();
+        return newData.getName() + "\n" + newData.getMail() + "\nSubmitted";
     }
 
     @PostMapping ("/index3")
     public String index3(@RequestBody IndexModel newIndexModel) {
-        return newIndexModel.getName() + "\n" + newIndexModel.getMail();
+        return newIndexModel.getName() + "\n" + newIndexModel.getMail() + "\nSubmitted";
+    }
+
+    @DeleteMapping ("/index4")
+    public String index4(@RequestParam String id) {
+        return "ID " +id+ " Deleted";
+    }
+
+    @PutMapping ("/index5")
+    public String index5(@RequestBody IndexModel newIndexModel) {
+        return newIndexModel.getName() + "\n" + newIndexModel.getMail() + "\nUpdated";
     }
 
 }
